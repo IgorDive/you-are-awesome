44493050bee016f4e28599ca002993ca3ae95e94
IgorDive <ihar.kazlovich@gmail.com>
2018-03-14 13:58:37 +0300

    modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
+const createEnumerableProperty = (prop) => {
+    return prop;
+};
+
+const createNotEnumerableProperty = (prop) => {
+    Object.defineProperty( Object.prototype, prop, {value: 'value', enumerable: false} );
+    return prop;
+};
+
 const createProtoMagicObject = () => {};
 const incrementor = () => {};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
+
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+
+const returnBackInSecond = ( item ) => { 
+    return new Promise ( ( resolve, reject ) => {
+        setTimeout( () => resolve( item ), 1100 );
+    } );
+};
+
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
